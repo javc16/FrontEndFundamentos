9a68d6e232b2cca5270c9f45e56d8633cd241b8e
DESKTOP-AICR3S2\alexe <javc1603@gmail.com>
2021-06-19 19:02:17 -0600

    cambios desarrollador

--- FrontProjectFundamentos/src/app/Components/desarrollador/desarrollador.component.ts
+++ FrontProjectFundamentos/src/app/Components/desarrollador/desarrollador.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Desarrollador } from 'src/app/Models/Desarrollador';
+import { DesarrolladorService } from 'src/app/Services/Desarrollador/desarrollador.service';
 
 @Component({
   selector: 'app-desarrollador',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./desarrollador.component.css']
 })
 export class DesarrolladorComponent implements OnInit {
-
-  constructor() { }
+  desarrolladores: Desarrollador[] = [];
+  displayedColumns: string[] = ['id', 'nombre', 'action'];
+  constructor(private router: Router, private _desarrolladorService: DesarrolladorService) { }
 
   ngOnInit(): void {
+    this._desarrolladorService.getAll().subscribe((data: any) => {
+      this.desarrolladores = data;
+    })
   }
 
+  create() {
+    this.router.navigate(['desarrollador/crear-desarrollador']);
+  }
 }
